872357ea2389db6340d3175752fa0ed453590d99
dot.exe <57018141+dot-exe@users.noreply.github.com>
2023-05-13 02:22:06 +0200

    fix models visibility

--- biblioteka/katalog/admin.py
+++ biblioteka/katalog/admin.py
@@ -5,18 +5,23 @@ from django.contrib import admin
 from .models import Autor, Gatunek, Ksiazka, InstacjaKsiazki, Bibliotekarz, Wydawca
 
 # admin.site.register(Autor)
-admin.site.register(Gatunek)
+# admin.site.register(Gatunek)
 # admin.site.register(Ksiazka)
 # admin.site.register(InstacjaKsiazki)
-admin.site.register(Bibliotekarz)
-admin.site.register(Wydawca)
+# admin.site.register(Bibliotekarz)
+# admin.site.register(Wydawca)
 
 
+# admin.site.register(Autor, AutorAdmin)
+
+@admin.register(Autor)
 class AutorAdmin(admin.ModelAdmin):
     list_display = ('nazwisko', 'imie', 'data_urodzenia', 'data_smierci')
 
 
-admin.site.register(Autor, AutorAdmin)
+@admin.register(Gatunek)
+class GatunekAdmin(admin.ModelAdmin):
+    display = 'nazwa'
 
 
 @admin.register(Ksiazka)
@@ -27,3 +32,14 @@ class KsiazkaAdmin(admin.ModelAdmin):
 @admin.register(InstacjaKsiazki)
 class InstancjaKsiazkiAdmin(admin.ModelAdmin):
     list_filter = ('status', 'data_zwrotu')
+
+
+@admin.register(Bibliotekarz)
+class BibliotekarzAdmin(admin.ModelAdmin):
+    list_display = ('nazwisko', 'imie', 'data_zatrudnienia', 'stanowisko')
+
+
+@admin.register(Wydawca)
+class WydawcaAdmin(admin.ModelAdmin):
+    list_display = ('nazwa', 'miasto', 'data_zalozenia', 'krs')
+#
